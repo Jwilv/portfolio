23edcda3d97975a3b1fd22e9a3d97f0cdd374503
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-25 16:38:39 -0300

    el component nieves esta listo

---
+++ src/components/nieve/index.js
@@ -0,0 +1,30 @@
+import '../../index.css'
+
+const body = document.querySelector('body');
+
+const timeAparicion = 200 ;
+
+const createNieve = ()=>{
+    let copo = document.createElement('i')
+    let xPosition = window.innerWidth * Math.random()
+    let size = (Math.random() * 8) + 2;
+    let zIndex = Math.round(Math.random()) * 100;
+    let Speed = Math.random() * 5;
+    let animacion = Math.random() * 10 + 5;
+
+    
+    copo.style.left = xPosition + 'px' ;
+    copo.style.width = size + 'px' ;
+    copo.style.height = size + 'px' ;
+    copo.style.zIndex = zIndex ;
+    copo.style.animationDelay = Speed + 's' ;
+    copo.style.animationDuration = animacion + 's'
+    
+    
+    body.appendChild(copo);
+    setTimeout( ()=>{
+        copo.remove()
+    }, animacion * 900)
+}
+
+setInterval(createNieve,timeAparicion);

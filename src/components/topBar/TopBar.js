7a7f387acbd1b7bc7329999835142db5e906fcaa
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-03 21:40:10 -0300

    creacion de barra de informacion con iconos de material ui

--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import './topBar.css'
+import {Person,Email} from '@mui/icons-material'
 
 const TopBarContainer = styled.div`
 width:100%;
@@ -19,6 +21,8 @@ justify-content:space-between;
 
 `
 const Left = styled.div`
+display:flex;
+align-items:center;
 `
 const Right = styled.div`
 `
@@ -27,9 +31,14 @@ font-size:40px;
 font-weight:700;
 text-decoration:none;
 color:black;
+margin-right: 40px;
 `
 
+
 const ItemConteiner = styled.div`
+display:flex;
+align-items:center;
+margin-left: 30px;
 `
 
 export const TopBar = () => {
@@ -38,6 +47,14 @@ export const TopBar = () => {
             <Wrapper>
                 <Left>
                     <Logo href='#intro'>Jwilv</Logo>
+                    <ItemConteiner>
+                    <Person className='icon'/>
+                    <span>+54 9 11 3904-1378</span>
+                    </ItemConteiner>
+                    <ItemConteiner>
+                    <Email className='icon'/>
+                    <span>juan.ignacio.wilvers@gmail.com</span>
+                    </ItemConteiner>
                     {/* <a href='#works'>fgfdgdfgd</a>
                 <a href='#portfolio'>fgfdgdfgd</a>
                 <a href='#contact'>fgfdgdfgd</a> */}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
-//import './components/nieve'
-//import { IntroPage } from './components/IntroPage/IntroPage';
-//import { Campfire } from './components/Campfire';
-
+import './components/nieve'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

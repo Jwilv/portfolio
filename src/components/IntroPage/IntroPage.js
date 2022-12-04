8d597451650c1015ee95dfdbfbb9170f6e702cfe
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-04 15:27:37 -0300

    agragado de flex y img de igloo

--- src/components/IntroPage/IntroPage.js
+++ src/components/IntroPage/IntroPage.js
@@ -1,15 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
-//import IglooIllustration from "../../assets/imgs/IglooIllustration.png"
-// <img src={IglooIllustration} alt='imagen que no carga' />
+import IglooIllustration from "../../assets/imgs/IglooIllustration.png"
+
 
 const IntroContainer = styled.div`
-background-color:green;
+background-color:white ;
+display:flex;
 `
 
+const Left = styled.div`
+flex:0.5;
+overflow:hidden;
+`
+
+const Right = styled.div`
+flex:0.5;
+`
+const ImageContainer = styled.div`
+width: 800px;
+height: 800px;
+background-color:blue;
+border-radius:50%;
+display:flex;
+align-items:center;
+justify-content:center;
+
+`
+
+const Img = styled.img`
+height:100%;
+`
 export const IntroPage = () => {
   return (
     <IntroContainer id='intro'>
+      <Left>
+        <ImageContainer>
+        <Img src={IglooIllustration} alt='imagen que no carga' />
+        </ImageContainer>
+      </Left>
+      <Right>
+
+      </Right>
     </IntroContainer>
   )
 }

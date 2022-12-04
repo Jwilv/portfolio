7309d7b193c10d4af9dc3166d4a62b2d71a26d5a
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-04 17:18:19 -0300

    cascaron de presentacion listo

--- src/components/IntroPage/IntroPage.js
+++ src/components/IntroPage/IntroPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import IglooIllustration from "../../assets/imgs/IglooIllustration.png"
+import flecha from "../../assets/imgs/flecha.png"
 
 
 const IntroContainer = styled.div`
@@ -15,6 +16,7 @@ overflow:hidden;
 
 const Right = styled.div`
 flex:0.5;
+position:relative;
 `
 const ImageContainer = styled.div`
 width: 800px;
@@ -30,6 +32,40 @@ justify-content:center;
 const Img = styled.img`
 height:100%;
 `
+
+const Wrapper = styled.div`
+width: 100%;
+height: 100%;
+padding-left:50px;
+display:flex;
+flex-direction:column;
+justify-content:center;
+`
+
+const H1 = styled.h1`
+font-size:60px;
+margin:10px 0;
+`
+
+const H2 = styled.h2`
+font-size:35px;
+`
+
+const H3 = styled.h3`
+font-size:35px;
+`
+
+const FlechaCointainer = styled.a`
+position:absolute;
+bottom: 10px;
+left:40%;
+`
+
+
+const Flecha = styled.img`
+width: 30px;
+`
+
 export const IntroPage = () => {
   return (
     <IntroContainer id='intro'>
@@ -39,6 +75,16 @@ export const IntroPage = () => {
         </ImageContainer>
       </Left>
       <Right>
+        <Wrapper>
+          <H2>que se pone aca ?</H2>
+          <H1>no puede hacer tarnco calor</H1>
+          <H3>freelance<span></span></H3>
+          <FlechaCointainer href='#intro'>
+            <Flecha src={flecha} alt='imagen que no carga' />
+          </FlechaCointainer>
+          
+
+        </Wrapper>
 
       </Right>
     </IntroContainer>

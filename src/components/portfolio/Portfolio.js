58d9fe8156bfea1118bd6661974bcffe5774bcfb
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-04 21:39:34 -0300

    se arreglo las imagenes

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,59 +1,58 @@
 import React from 'react'
-import styled from 'styled-components'
+import './portfolio.scss'
 
-const PortfolioContainer = styled.div`
-background-color:white;
-display:flex;
-flex-direction:column;
-align-items:center;
 
-`
-
-const Title = styled.h1`
-font-size:50px;
-`
-
-const Container = styled.div`
-
-`
-
-const Item = styled.div`
-
-`
-
-const Lista = styled.ul`
-margin: 10px;
-padding:0;
-list-style:none;
-display:flex;
-`
-
-const Items = styled.li`
-font-size:14px;
-margin-right: 50px;
-padding: 0.7;
-`
 
 export const Portfolio = () => {
     return (
-        <PortfolioContainer id='portfolio'>
+        <div className='PortfolioContainer' id='portfolio'>
 
-            <Title>Portfolio</Title>
-            <Lista>
-                <li>Featured</li>
+            <h1>Portfolio</h1>
+            <ul>
+                <li className='active'>Featured</li>
                 <li>Web App</li>
                 <li>Android App</li>
                 <li>Designs</li>
-            </Lista>
-            <Container>
-            <Item>
-                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmKvoSz5GPO4YHunnOXkaMJVyNX6Dn-8McfQ&usqp=CAU' alt='imagen de prueba' />
-                <h3>imagen de prueba</h3>
+            </ul>
 
-            </Item>
+            <div className="Container">
 
-            </Container>
+                <div className='Item'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCzGjj6cpYkZ7fUDtoPSxBfdrUTsbdCYpTA&usqp=CAU" alt='imagen de prueba' />
+                    <h3>imagen de prueba</h3>
+                </div>
 
-        </PortfolioContainer>
+                
+                <div className='Item'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCzGjj6cpYkZ7fUDtoPSxBfdrUTsbdCYpTA&usqp=CAU" alt='imagen de prueba' />
+                    <h3>imagen de prueba</h3>
+                </div>
+
+                
+                <div className='Item'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCzGjj6cpYkZ7fUDtoPSxBfdrUTsbdCYpTA&usqp=CAU" alt='imagen de prueba' />
+                    <h3>imagen de prueba</h3>
+                </div>
+
+                <div className='Item'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCzGjj6cpYkZ7fUDtoPSxBfdrUTsbdCYpTA&usqp=CAU" alt='imagen de prueba' />
+                    <h3>imagen de prueba</h3>
+                </div>
+
+                
+                <div className='Item'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCzGjj6cpYkZ7fUDtoPSxBfdrUTsbdCYpTA&usqp=CAU" alt='imagen de prueba' />
+                    <h3>imagen de prueba</h3>
+                </div>
+
+                
+                <div className='Item'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCzGjj6cpYkZ7fUDtoPSxBfdrUTsbdCYpTA&usqp=CAU" alt='imagen de prueba' />
+                    <h3>imagen de prueba</h3>
+                </div>
+
+            </div>
+
+        </div>
     )
 }

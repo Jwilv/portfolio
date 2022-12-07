5eefdb89888c1e3bdc63e9b8ff2d3b5226d28955
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-07 15:28:58 -0300

    integracion de data en <Portfolio /> y ceacion de useEffect y useState

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { PortfolioList } from '../portfolioList/PortfolioList';
 import './portfolio.scss'
+import {
+    featuredPortfolio,
+    WebAppPortfolio,
+    movileAppPortfolio,
+    designsPortfolio,
+} from '../../data'
 
 
 
 export const Portfolio = () => {
 
     const [select, setSelect] = useState('featured')
+    const [data, setData] = useState([])
 
     const list = [{
         id: 'featured',
@@ -24,16 +31,36 @@ export const Portfolio = () => {
         id: 'designs',
         title: 'Designs',
     },];
+
+    useEffect(() => {
+
+        switch (select) {
+            case 'featured':
+                setData(featuredPortfolio);
+                break;
+            case 'web':
+                setData(WebAppPortfolio);
+                break;
+            case 'movile':
+                setData(movileAppPortfolio);
+                break;
+            case 'designs':
+                setData(designsPortfolio);
+                break;
+            default: setData(featuredPortfolio);
+        }
+    }, [select])
+
     return (
         <div className='PortfolioContainer' id='portfolio'>
 
             <h1>Portfolio</h1>
             <ul>
-                {list.map( ({title,id}) =>(
+                {list.map(({ title, id }) => (
                     <PortfolioList key={id} title={title}
-                    active={select === id}
-                    setSelect={setSelect}
-                    id={id}/>
+                        active={select === id}
+                        setSelect={setSelect}
+                        id={id} />
                 ))}
 
             </ul>

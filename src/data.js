4c18c82d4ba2efd10dd133f58477fd8d1ba7cb90
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-07 15:13:25 -0300

    data test creada

---
+++ src/data.js
@@ -0,0 +1,140 @@
+const urlTest1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCzGjj6cpYkZ7fUDtoPSxBfdrUTsbdCYpTA&usqp=CAU';
+
+const urlTest2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRufEKWu3Siah3tU8NA99-WE27KujYfiV3Gyg&usqp=CAU';
+
+const urlTest3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-uNQePqGp9sRqFOX82DdvlSWRAuN9JS-jXQ&usqp=CAU';
+
+const urlTest4 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCVvfmON7SJK7p2_pBigHJpShsNW911Bh3Ug&usqp=CAU';
+
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: `test 1`,
+        img: urlTest1,
+    },
+    {
+        id: 2,
+        title: `test 2`,
+        img: urlTest1,
+    },
+    {
+        id: 3,
+        title: `test 3`,
+        img: urlTest1,
+    },
+    {
+        id: 4,
+        title: `test 4`,
+        img: urlTest1,
+    },
+    {
+        id: 5,
+        title: `test 5`,
+        img: urlTest1,
+    },
+    {
+        id: 6,
+        title: `test 6`,
+        img: urlTest1,
+    },
+]
+
+export const WebAppPortfolio = [
+    {
+        id: 1,
+        title: `test 1`,
+        img: urlTest2,
+    },
+    {
+        id: 2,
+        title: `test 2`,
+        img: urlTest2,
+    },
+    {
+        id: 3,
+        title: `test 3`,
+        img: urlTest2,
+    },
+    {
+        id: 4,
+        title: `test 4`,
+        img: urlTest2,
+    },
+    {
+        id: 5,
+        title: `test 5`,
+        img: urlTest2,
+    },
+    {
+        id: 6,
+        title: `test 6`,
+        img: urlTest2,
+    },
+]
+
+export const movileAppPortfolio = [
+    {
+        id: 1,
+        title: `test 1`,
+        img: urlTest3,
+    },
+    {
+        id: 2,
+        title: `test 2`,
+        img: urlTest3,
+    },
+    {
+        id: 3,
+        title: `test 3`,
+        img: urlTest3,
+    },
+    {
+        id: 4,
+        title: `test 4`,
+        img: urlTest3,
+    },
+    {
+        id: 5,
+        title: `test 5`,
+        img: urlTest3,
+    },
+    {
+        id: 6,
+        title: `test 6`,
+        img: urlTest3,
+    },
+]
+
+export const designsPortfolio = [
+    {
+        id: 1,
+        title: `test 1`,
+        img: urlTest4,
+    },
+    {
+        id: 2,
+        title: `test 2`,
+        img: urlTest4,
+    },
+    {
+        id: 3,
+        title: `test 3`,
+        img: urlTest4,
+    },
+    {
+        id: 4,
+        title: `test 4`,
+        img: urlTest4,
+    },
+    {
+        id: 5,
+        title: `test 5`,
+        img: urlTest4,
+    },
+    {
+        id: 6,
+        title: `test 6`,
+        img: urlTest4,
+    },
+]
+

e5fe383e2a5f0565e5e0d3b4204607077a162c79
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-08 16:26:49 -0300

    se estilo el lado izquierdo de el carrucel

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -8,10 +8,21 @@ export const Works = () => {
         <div className='worksContainer' id='works'>
             <div className='slider'>
                 <div className='container'>
-                <div className='item'>
-                <div className='left'></div>
-                <div className='right'></div>
-                </div>
+                    <div className='item'>
+                        <div className='left'>
+                            <div className='leftContainer'>
+                                <div className='imgCointainer'>
+                                    {/* <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb_tu7GgJWDljnoor35A4-jwLC93qS6VGPfw&usqp=CAU'
+                                        alt='imagen que no carga' /> */}
+                                </div>
+                                <h2>titulo de prueba</h2>
+                                <p>gjfdhgj fdhgj jhs jghjsdfdhgjfshjkgsfhdjkghjdfshgjhfsdjkghdfjksg
+                                </p>
+                                <span>Proyects</span>
+                            </div>
+                        </div>
+                        <div className='right'></div>
+                    </div>
                 </div>
             </div>
 

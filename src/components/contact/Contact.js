451fdb727558f06364334fcd3619b4d37e26e420
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-11 19:00:23 -0300

    bases de form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,6 +5,10 @@ import pinguinoContact from '../../assets/imgs/pinguinoContact.png'
 
 
 export const Contact = () => {
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+    }
     return (
         <div className='ContactContainer' id='contact'>
 
@@ -13,7 +17,7 @@ export const Contact = () => {
             </div>
             <div className='right'>
                 <h2>Contact</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type='text' placeholder='Email'/>
                     <textarea placeholder='Message'></textarea>
                     <button type='submit'>Send</button>

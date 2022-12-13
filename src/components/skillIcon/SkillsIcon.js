2a3d43a34cdaca2b0de8c22d9665737f72588670
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-13 19:52:19 -0300

    creacion de dot

--- src/components/skillIcon/SkillsIcon.js
+++ src/components/skillIcon/SkillsIcon.js
@@ -7,7 +7,8 @@ export const SkillsIcon = () => {
     return (
         <div className='skillsIconContainer'>
             <div className='card'>
-                <div className='percent' style={}>
+                <div className='percent' >
+                    <div className='dot'></div>
                     <svg>
                         <circle cx='70' cy='70' r='70' ></circle>
                         <circle cx='70' cy='70' r='70' ></circle>

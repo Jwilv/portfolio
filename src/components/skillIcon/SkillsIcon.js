f49ba7fd91f4d845e4d40d8f489694a907d895b2
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-14 17:43:04 -0300

    se termino los estilos en ccs y se crearon las animaciones

--- src/components/skillIcon/SkillsIcon.js
+++ src/components/skillIcon/SkillsIcon.js
@@ -13,6 +13,10 @@ export const SkillsIcon = () => {
                         <circle cx='70' cy='70' r='70' ></circle>
                         <circle cx='70' cy='70' r='70' ></circle>
                     </svg>
+                    <div className='number'>
+                        <h2>80<span>%</span></h2>
+                        <p>Html</p>
+                    </div>
                 </div>
             </div>
         </div>
